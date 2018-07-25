73b0e7e368ff8f12bd6e6a59f55ee18ba3e2f514
JimKontosfyris <jimkontosfyris@gmail.com>
2018-07-25 17:55:34 +0300

    25/7: Excel button

--- public/dashboard/dashboardtest.js
+++ public/dashboard/dashboardtest.js
@@ -122,6 +122,27 @@ function getFineData () {
         FineLatLong.push([CarPlate, Lat, Lon]);
     });
     populateDatatable();
+
+    // Create workbook
+    var wb= XLSX.utils.book_new();
+    wb.Props= {
+      Title: "Information"
+    };
+    wb.SheetNames.push("Πληροφορίες");
+    var ws = XLSX.utils.aoa_to_sheet(FineTable);
+    wb.Sheets["Πληροφορίες"]= ws;
+    // Exporting workbook for download
+    var wbout= XLSX.write(wb, {booktype: 'xlsx', type: 'binary'});
+    function s2ab(s) {
+                var buf = new ArrayBuffer(s.length); //convert s to arrayBuffer
+                var view = new Uint8Array(buf);  //create uint8array as viewer
+                for (var i=0; i<s.length; i++) view[i] = s.charCodeAt(i) & 0xFF; //convert to octet
+                return buf;
+    }
+    $("#xlsx").click(function(){
+       saveAs(new Blob([s2ab(wbout)],{type:"application/octet-stream"}), 'Information.xlsx');
+    });
+    //
     return FineTable;
   });
 }
